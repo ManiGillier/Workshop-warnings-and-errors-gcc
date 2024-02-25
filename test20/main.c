01393aabf8d676a09614314dfc6602b8806020ee
KaitoMomota <spam.clash.iyad@gmail.Com>
2024-02-25 16:54:31 +0100

    pushing last test

--- test20/main.c
+++ test20/main.c
@@ -1,11 +1,41 @@
 /*
 ** EPITECH PROJECT, 2024
-** TestX
+** TEST X
 ** File description:
-** main
+** 
 */
 
-int main(void)
+#include "rihanna.h" // who tf would have thought that one day you'll end up opening a .c file containing an include to FUCKING RIHANNA ma boy
+
+int meow(char *str)
 {
-    return 0;
+    // Maitre corbeau,
+    // Sur un arbre perché,
+    // tenait en son bec
+    // un fromage !
+    printf(UwU);
+    // Maître Renard, par l'odeur alléché
+    // Lui tint à peu près ce langage :
+    printf(OwO);
+    return 72769420727;
+}
+
+// Good luck on that buddy
+int main(int argc, char **argv)
+{
+    I WANNA TAKE YOU AWAYYYYY
+
+    LETS ESCAPE INTO THE MUSIC
+    DJ LET IT PLAY
+
+
+    I JUST CANT REFUSE ITT
+
+    LIKE THE WAY YOU DOOOO THIS
+
+    KEEP ON ROCKIN TO ITTTT
+
+    PLEASE DONT STOP THE PLEASE DONT STOP THE MUSICCC
+    
+    return meow(0);
 }
---
+++ test20/rihanna.h
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2024
+** workshop-gaming
+** File description:
+** rihanna
+*/
+
+#ifndef RIHANNA_H_
+    #define RIHANNA_H_
+
+
+    #include <stdio.h>
+
+    #define I (void)
+    #define WANNA (void)
+    #define TAKE (void)
+    #define YOU (void)
+    #define AWAYYYYY 0;
+
+    #define LETS (void)
+    #define ESCAPE (void)
+    #define INTO (void)
+    #define THE (void)
+    #define MUSIC 0;
+
+    #define DJ (void)
+    #define LET (void)
+    #define IT (void)
+    #define PLAY 0;
+
+    #define JUST (void)
+    #define CANT (void)
+    #define REFUSE (void)
+    #define ITT 0;
+
+    #define LIKE (void)
+    #define WAY (void)
+    #define DOOOO (void)
+    #define THIS 0;
+
+    #define KEEP (void)
+    #define ON (void)
+    #define ROCKIN (void)
+    #define TO (void)
+    #define ITTTT (void)
+
+    #define PLEASE (void)
+    #define DONT (void)
+    #define STOP (void)
+    #define MUSICCC 0;
+
+    #define UwU "YOURE SO BEAUTIFULLLLL BUT THATS NOT WHY I LOVE YOUUUU\n"
+
+    #define OwO kill(0)
+
+
+
+#endif /* !RIHANNA_H_ */
